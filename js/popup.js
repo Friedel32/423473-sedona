d25267368dad71c04a0dc40cc32c9d5ee54eb8f4
Екатерина <katerina_32@mail.ru>
2017-05-14 10:08:52 +0300

    Исправления

--- js/popup.js
+++ js/popup.js
@@ -5,3 +5,11 @@ link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.toggle("hotel-form-show");
    });
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+  if (popup.classList.contains("hotel-form-show")) {
+  popup.classList.remove("hotel-form-show");
+  }
+  }
+  });
