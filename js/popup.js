e46e472c8fff3dce84b4dbba98003fb0939dd5e9
Екатерина <katerina_32@mail.ru>
2017-05-14 13:00:19 +0300

    Исправления

--- js/popup.js
+++ js/popup.js
@@ -4,12 +4,12 @@ var close = document.querySelector(".hotel-form-close");
 link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.toggle("hotel-form-show");
-   });
+});
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-  if (popup.classList.contains("hotel-form-show")) {
-  popup.classList.remove("hotel-form-show");
+    if (popup.classList.contains("hotel-form-show")) {
+      popup.classList.remove("hotel-form-show");
+    }
   }
-  }
-  });
+});
